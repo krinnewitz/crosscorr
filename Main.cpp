9db049068345545ca9c9ca5286c8f03d31e12866
Kim Oliver Rinnewitz <krinnewitz@uos.de>
2012-06-05 19:00:05 +0200

    Fixed cross correlation function and added function to interprete the result

--- Main.cpp
+++ Main.cpp
@@ -17,7 +17,6 @@
 
 using namespace std;
 
-
 /**
  * \brief 	Implementation of the cross correlation function using fourier transformation.
  *		This implementation is quite fast and may be used for productive jobs. Cross
@@ -31,38 +30,91 @@ using namespace std;
  * \param	dst	The destination to store the correlation values in. The result is NOT
 			normed.
  */
-void crosscorrDFT(const cv::Mat &img1, const cv::Mat &img2, cv::Mat &dst)
+void crosscorrDFT(const cv::Mat& img1, const cv::Mat& img2, cv::Mat& C)
 {
-
 	//Convert image1 from unsigned char to float matrix
-	cv::Mat fImg1;
-	img1.convertTo(fImg1, CV_32FC1);
+	cv::Mat A;
+	img1.convertTo(A, CV_32FC1);
+	//Subtract the mean
+	cv::Mat meanA(A.size(), A.type(), cv::mean(A));
+	cv::subtract(A, meanA, A);
+	
 
 	//Convert image2 from unsigned char to float matrix
-	cv::Mat fImg2;
-	img2.convertTo(fImg2, CV_32FC1);
+	cv::Mat B;
+	img2.convertTo(B, CV_32FC1);
+	//Subtract the mean
+	cv::Mat meanB(B.size(), B.type(), cv::mean(B));
+	cv::subtract(B, meanB, B);
 
-	//Calculate the optimal size for the dft output.
-	//This increases speed.
+	// reallocate the output array if needed
+	C.create(abs(A.rows - B.rows)+1, abs(A.cols - B.cols)+1, A.type());
+
+	// compute the size of DFT transform
 	cv::Size dftSize;
-	dftSize.width = cv::getOptimalDFTSize(img1.cols + img2.cols - 1);
-	dftSize.height = cv::getOptimalDFTSize(img1.rows + img2.rows -1);
+	dftSize.width = cv::getOptimalDFTSize(A.cols + B.cols - 1);
+	dftSize.height = cv::getOptimalDFTSize(A.rows + B.rows - 1);
 
-	//prepare the destination for the first dft
-	cv::Mat dft1 = cv::Mat(dftSize, CV_32FC1, cv::Scalar::all(0));
-	//transform the image into the frequency domain
-	cv::dft(fImg1, dft1);
+	// allocate temporary buffers and initialize them with 0’s
+	cv::Mat tempA(dftSize, A.type(), cv::Scalar::all(0));
+	cv::Mat tempB(dftSize, B.type(), cv::Scalar::all(0));
 
-	//prepare the destination for the second dft
-	cv::Mat dft2 = cv::Mat(dftSize, CV_32FC1, cv::Scalar::all(0));
-	//transform the image into the frequency domain
-	cv::dft(fImg2, dft2);
+	// copy A and B to the top-left corners of tempA and tempB, respectively
+	cv::Mat roiA(tempA, cv::Rect(0,0,A.cols,A.rows));
+	A.copyTo(roiA);
+	cv::Mat roiB(tempB, cv::Rect(0,0,B.cols,B.rows));
+	B.copyTo(roiB);
+
+	// now transform the padded A & B in-place;
+	// use "nonzeroRows" hint for faster processing
+	cv::dft(tempA, tempA, 0, A.rows);
+	cv::dft(tempB, tempB, 0, B.rows);
 
 	//calculate DFT1 * DFT2* (don't mind the fourth parameter. It is ignored)
-	cv::mulSpectrums(dft1, dft2, dst, cv::DFT_INVERSE, true);
-	//transform the result back to the image domain 
-	cv::dft(dst, dst, cv::DFT_INVERSE | cv::DFT_SCALE);
+	cv::mulSpectrums(tempA, tempB, tempA, cv::DFT_INVERSE, true);
+
+	// transform the product back from the frequency domain.
+	// Even though all the result rows will be non-zero,
+	// we need only the first C.rows of them, and thus we
+	// pass nonzeroRows == C.rows
+	cv::dft(tempA, tempA, cv::DFT_INVERSE + cv::DFT_SCALE, C.rows);
+
+	// now copy the result back to C.
+	tempA(cv::Rect(0, 0, C.cols, C.rows)).copyTo(C);
 }
+
+
+/**
+ * \brief Searches the maximum value in the given cross correlation.
+ *
+ * \param input	The cross correlation of two images
+ * \param resX	The x coordinate of the maximum value
+ * \param resY	The y coordinate of the maximum value
+ *
+ * \return The maximum value.
+ */
+double getMax(const cv::Mat &input, unsigned int &resX, unsigned int &resY)
+{
+	const float epsilon = 0.00005;
+
+	cv::Mat_<float>& ptrCC = (cv::Mat_<float>&)input;
+
+	resX = 0;
+	resY = 0;
+	
+	for (unsigned int x = 0; x < input.size().width; x++)
+	{
+		for (unsigned int y = 0; y < input.size().height; y++)
+		{
+			if (ptrCC(y, x) > ptrCC(resY, resX) + epsilon)
+			{
+				resY = y;	
+				resX = x;
+			}
+		}
+	}
+	return ptrCC(resY, resX);
+} 
 
 
 int main (int argc, char** argv)
@@ -80,8 +132,14 @@ int main (int argc, char** argv)
 
 		cv::Mat crossc;
 
+		//calculate cross correlation
 		crosscorrDFT(src1, src2, crossc);
-		//TODO	
+
+		//find highest correlation coordinates
+		unsigned int x, y;
+		double result = getMax(crossc, x, y);
+
+		cout<<result<<" "<<x<<" "<<y<<endl;		
 
 		return EXIT_SUCCESS;
 	}
